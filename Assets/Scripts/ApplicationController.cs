b3324c0bed97379ce5b2a28749eb2e16d4000c50
ByMedion <dan.ef1999@gmail.com>
2022-05-17 02:16:03 +0300

    Mock AdsController added to ApplicationController

--- Assets/Scripts/ApplicationController.cs
+++ Assets/Scripts/ApplicationController.cs
@@ -1,3 +1,4 @@
+using Dainty.Ads;
 using Dainty.UI;
 using UnityEngine;
 
@@ -7,12 +8,14 @@ public class ApplicationController : Singleton<ApplicationController>
     public UiManager UiManager => _uiManager;
     public UiRoot UiRoot => _uiRoot;
     public TopPanelController TopPanelController => _topPanelController;
+    public IAdsController AdsController => _adsController;
 
     [SerializeField] private Camera _camera;
     [SerializeField] private UiRoot _uiRoot;
     [SerializeField] private UiManagerSettings _uiManagerSettings;
     [SerializeField] private TopPanelController _topPanelController;
     private UiManager _uiManager;
+    private IAdsController _adsController;
 
     protected override void Awake()
     {
@@ -22,5 +25,17 @@ public class ApplicationController : Singleton<ApplicationController>
 
         _uiManager = new UiManager(_uiRoot, _uiManagerSettings);
         _uiManager.Open<MainController>(false, WindowTransition.None);
+
+        _adsController = CreateAdsController();
+    }
+
+    private IAdsController CreateAdsController()
+    {
+        var obj = new GameObject("MockAdsController");
+        DontDestroyOnLoad(obj);
+
+        IAdsController adsController = obj.AddComponent<EditorAdsController>();
+        adsController.Initialize(true);
+        return adsController;
     }
 }
