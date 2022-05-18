fdbb01bc2decdc493a742601e178f18514ec1c99
ByMedion <dan.ef1999@gmail.com>
2022-05-18 21:45:14 +0300

    ShopWindow: rewarded coins logic added

--- Assets/MemoryArt/Scripts/UI/Windows/Shop/ShopWindowController.cs
+++ Assets/MemoryArt/Scripts/UI/Windows/Shop/ShopWindowController.cs
@@ -4,11 +4,19 @@ namespace MemoryArt.UI.Windows
 {
     public class ShopWindowController : AWindowController<ShopWindowView>
     {
+        private const int FREE_COINS_AMOUNT = 10;
+
         public override string WindowId { get; }
+
+        protected override void OnInitialize()
+        {
+            view.SetFreeCoinsAmount(FREE_COINS_AMOUNT);
+        }
 
         protected override void OnSubscribe()
         {
             view.CloseButtonClick += ViewOnCloseButtonClick;
+            view.FreeCoinsClick += ViewOnFreeCoinsClick;
         }
 
         protected override void OnUnSubscribe()
@@ -19,6 +27,17 @@ namespace MemoryArt.UI.Windows
         private void ViewOnCloseButtonClick()
         {
             uiManager.Back();
+        }
+
+        private void ViewOnFreeCoinsClick()
+        {
+            ApplicationController.Instance.AdsController.ShowRewarded(null, success =>
+            {
+                if (success)
+                {
+                    MoneyController.Instance.AddMoney(FREE_COINS_AMOUNT);
+                }
+            }, null);
         }
     }
 }
--- Assets/MemoryArt/Scripts/UI/Windows/Shop/ShopWindowView.cs
+++ Assets/MemoryArt/Scripts/UI/Windows/Shop/ShopWindowView.cs
@@ -1,5 +1,6 @@
 using System;
 using Dainty.UI.WindowBase;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,13 +11,19 @@ namespace MemoryArt.UI.Windows
         [SerializeField] private Button _closeButton;
         [SerializeField] private Button _backgroundShadeButton;
 
+        [Space] [SerializeField] private Button _freeCoinsButton;
+        [SerializeField] private TMP_Text _freeCoinsAmountText;
+
         public event Action CloseButtonClick;
+        public event Action FreeCoinsClick;
 
         protected override void OnSubscribe()
         {
             base.OnSubscribe();
             _closeButton.onClick.AddListener(OnCloseClicked);
             _backgroundShadeButton.onClick.AddListener(OnCloseClicked);
+
+            _freeCoinsButton.onClick.AddListener(() => FreeCoinsClick?.Invoke());
         }
 
         protected override void OnUnSubscribe()
@@ -24,11 +31,18 @@ namespace MemoryArt.UI.Windows
             base.OnSubscribe();
             _backgroundShadeButton.onClick.RemoveAllListeners();
             _closeButton.onClick.RemoveAllListeners();
+
+            _freeCoinsButton.onClick.RemoveAllListeners();
         }
 
         private void OnCloseClicked()
         {
             CloseButtonClick?.Invoke();
+        }
+
+        public void SetFreeCoinsAmount(int freeCoinsAmount)
+        {
+            _freeCoinsAmountText.text = freeCoinsAmount.ToString();
         }
     }
 }
